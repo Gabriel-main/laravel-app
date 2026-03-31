ff05da215c5dc5589d1c02f9fe7d9d5d35ecd845
Gabriel <gcolmenares19@hotmail.com>
2026-03-30 21:23:38 -0400

    Se coloco logo en el admin y navegacion

--- resources/views/components/web/navbar.blade.php
+++ resources/views/components/web/navbar.blade.php
@@ -3,13 +3,7 @@
 
         <div class="flex lg:flex-1">
             <a href="{{ route('ventor.pagina-index') }}" class="-m-1.5 p-1.5 flex items-center gap-3">
-                <div
-                    class="w-9 h-9 bg-indigo-600 rounded-xl flex items-center justify-center shadow-lg shadow-indigo-200">
-                    <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
-                    </svg>
-                </div>
+                <x-application-logo/>
                 <span class="text-xl font-bold tracking-tight text-slate-900">Ventor</span>
             </a>
         </div>
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -29,9 +29,19 @@ new class extends Component
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-6 sm:-my-px sm:ms-34 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                        {{ __('Inicio') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Mis gastos') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Mis presupuestos') }}
+                    </x-nav-link>
+                    
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                        {{ __('Analisis') }}
                     </x-nav-link>
                 </div>
             </div>
